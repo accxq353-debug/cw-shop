f6a6070a314f1cea3d051d69cd6d53b37fca924f
Westass <accxq353@gmail.com>
2026-09-23 03:11:56 +0300

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  serverExternalPackages: ["discord.js", "@discordjs/ws", "@discordjs/rest"],
-};
-
-export default nextConfig;
